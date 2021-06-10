c7091af4d9fc130e508ee0d26e466062d6941f87
1NT18IS110 <82625478+1NT18IS110@users.noreply.github.com>
2021-06-10 08:14:04 +0530

    Add files via upload

---
+++ pay_raise.java
@@ -0,0 +1,64 @@
+package pay_raise_package;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.FileInputFormat;
+import org.apache.hadoop.mapred.FileOutputFormat;
+import org.apache.hadoop.mapred.JobClient;
+import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.mapred.MapReduceBase;
+import org.apache.hadoop.mapred.Mapper;
+import org.apache.hadoop.mapred.OutputCollector;
+import org.apache.hadoop.mapred.Reducer;
+import org.apache.hadoop.mapred.Reporter;
+import org.apache.hadoop.mapred.TextInputFormat;
+import org.apache.hadoop.mapred.TextOutputFormat;
+
+public class pay_raise {
+	public static class Map extends MapReduceBase implements Mapper<LongWritable, Text, Text, IntWritable> {
+		private final static IntWritable one = new IntWritable(1);
+		//private Text word = new Text();
+		
+		public void map(LongWritable key, Text value, OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException {
+			String mstring = value.toString();// text to string
+			String[] Tokens=mstring.split(",");
+			if(Tokens[5].equals("YES"))
+			output.collect(new Text(Tokens[5]),one);}
+			
+		}
+	public static class Reduce extends MapReduceBase implements Reducer<Text, IntWritable, Text, IntWritable> {
+		public void reduce(Text key, Iterator<IntWritable> values, OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException {
+			int count= 0;
+			Text word = new Text("YES");
+			while (values.hasNext()) {
+				
+			         count += values.next().get();
+		       	
+			}
+			output.collect(new Text("Number of people eligible for pay raise: "), new IntWritable(count));
+			}
+		}
+	
+
+	public static void main(String[] args) throws Exception {
+		JobConf conf = new JobConf(pay_raise.class);
+		conf.setJobName("number of employees eligible for pay raise");
+		conf.setOutputKeyClass(Text.class);
+		conf.setOutputValueClass(IntWritable.class);
+		conf.setMapperClass(Map.class);
+		conf.setCombinerClass(Reduce.class);
+		conf.setReducerClass(Reduce.class);
+		conf.setInputFormat(TextInputFormat.class);
+		conf.setOutputFormat(TextOutputFormat.class); 
+		
+		FileInputFormat.setInputPaths(conf, new Path(args[0]));
+		FileOutputFormat.setOutputPath(conf, new Path(args[1]));
+		JobClient.runJob(conf);   
+	}
+
+}
